f1acd1aa11fa3a6fc644a88fb6a660551fb672df
ramnique singh <ramnique@kobol.me>
2017-01-06 13:22:16 +0530

    Make redis connection configurable

--- src/redis.js
+++ src/redis.js
@@ -1,10 +1,14 @@
-var redis = require('redis');
-var bb    = require('bluebird');
+var redis  = require('redis');
+var bb     = require('bluebird');
+var config = require('./config');
 
 bb.promisifyAll(redis.RedisClient.prototype);
 bb.promisifyAll(redis.Multi.prototype);
 
-var client = redis.createClient();
+var client = redis.createClient({
+	host: config.redis.host,
+	port: config.redis.port
+});
 
 client.on('error', function (err) {
     console.error(err.stack);
