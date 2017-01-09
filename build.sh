7c5e3de8089958e3af8a98834840c7660f5603d1
ramnique singh <ramnique@kobol.me>
2017-01-09 09:39:59 +0530

    Remove protobuf lib

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 npm install
-./node_modules/.bin/pbjs types.proto -t commonjs > types.js
--- types.js
+++ types.js
@@ -1,123 +1,35 @@
-module.exports = require("protobufjs").newBuilder({})['import']({
-    "package": null,
-    "messages": [
-        {
-            "name": "VariationT",
-            "fields": [
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "id",
-                    "id": 1
-                },
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "experiment_id",
-                    "id": 2
-                },
-                {
-                    "rule": "optional",
-                    "type": "string",
-                    "name": "name",
-                    "id": 3
-                },
-                {
-                    "rule": "optional",
-                    "type": "double",
-                    "name": "split_percent",
-                    "id": 4
-                },
-                {
-                    "rule": "optional",
-                    "type": "bool",
-                    "name": "is_deleted",
-                    "id": 5
-                },
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "create_time",
-                    "id": 6
-                },
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "update_time",
-                    "id": 7
-                }
-            ]
-        },
-        {
-            "name": "ExperimentT",
-            "fields": [
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "id",
-                    "id": 1
-                },
-                {
-                    "rule": "optional",
-                    "type": "string",
-                    "name": "name",
-                    "id": 2
-                },
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "version",
-                    "id": 3
-                },
-                {
-                    "rule": "optional",
-                    "type": "double",
-                    "name": "exposure_percent",
-                    "id": 4
-                },
-                {
-                    "rule": "optional",
-                    "type": "bool",
-                    "name": "is_active",
-                    "id": 5
-                },
-                {
-                    "rule": "optional",
-                    "type": "bool",
-                    "name": "is_usr_participating",
-                    "id": 6
-                },
-                {
-                    "rule": "optional",
-                    "type": "VariationT",
-                    "name": "usr_variation",
-                    "id": 7
-                },
-                {
-                    "rule": "optional",
-                    "type": "bool",
-                    "name": "is_deleted",
-                    "id": 8
-                },
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "create_time",
-                    "id": 9
-                },
-                {
-                    "rule": "optional",
-                    "type": "uint32",
-                    "name": "update_time",
-                    "id": 10
-                },
-                {
-                    "rule": "repeated",
-                    "type": "VariationT",
-                    "name": "variations",
-                    "id": 11
-                }
-            ]
-        }
-    ]
-}).build();
+function VariationT(data) {
+	data               = data || {};
+	this.id            = data.id ? Number(data.id) : 0;
+	this.experiment_id = data.experiment_id ? Number(data.experiment_id) : 0;
+	this.name          = data.name ? String(data.name) : '';
+	this.split_percent = data.split_percent ? Number(data.split_percent) : 0;
+	this.is_deleted    = Boolean(data.is_deleted);
+	this.create_time   = data.create_time ? Number(data.create_time) : 0;
+	this.update_time   = data.update_time ? Number(data.update_time) : 0;
+}
+
+function ExperimentT(data) {
+	data                      = data || {};
+	this.id                   = data.id ? Number(data.id) : 0;
+	this.name                 = data.name ? String(data.name) : '';
+	this.version              = data.version ? Number(data.version) : 0;
+	this.exposure_percent     = data.exposure_percent ? Number(data.exposure_percent) : 0;
+	this.is_active            = Boolean(data.is_active);
+	this.is_usr_participating = Boolean(data.is_usr_participating);
+	this.usr_variation        = data.usr_variation ? new VariationT(data.usr_variation) : null;
+	this.is_deleted           = Boolean(data.is_deleted);
+	this.create_time          = data.create_time ? Number(data.create_time) : 0;
+	this.update_time          = data.update_time ? Number(data.update_time) : 0;
+	this.variations           = [];
+	if (data.variations && data.variations.length) {
+		this.variations = data.variations.map(function (vrtn) {
+			return new VariationT(vrtn);
+		});
+	}
+}
+
+module.exports = {
+	VariationT : VariationT,
+	ExperimentT: ExperimentT
+};
