67aff0af22897def7a6bc0832fffa4dcb389cf24
arawind <aravind.p@stayzilla.com>
2017-03-06 10:47:53 +0530

    Restrict CORS origins

--- src/expressapp.js
+++ src/expressapp.js
@@ -10,7 +10,7 @@ var app       = express();
 app.use(bp.json());
 app.use(cp());
 app.use(function(req, res, next) {
- res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', config.app.cors_allowed_origins);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
