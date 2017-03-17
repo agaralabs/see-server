9564e576feb6303a1357cc266a80bc7d62dd96b3
arawind <aravind.p@stayzilla.com>
2017-03-17 15:09:14 +0530

    Make one single dist directory

--- .bin/build.sh
+++ .bin/build.sh
@@ -8,7 +8,9 @@ PROJECTDIR="${SRCDIR}/.."
 print_task_name "Set CWD"
 echo_and_eval "cd ${SERVERDIR}"
 
-echo_and_eval "rsync -r --delete --exclude=dist --exclude=node_modules --exclude=README.md --exclude=.* --exclude=tests ${PROJECTDIR}/ ${DISTDIR}"
+echo_and_eval "rsync -r --delete \
+    --exclude=dist --exclude=node_modules --exclude=*.md --exclude=.* --exclude=tests --exclude=data-processor \
+    ${SERVERDIR}/ ${SERVERDIR}/dist"
 
 print_task_name "Build data processor jar"
 echo_and_eval "cd ${SERVERDIR}/data-processor"
@@ -21,5 +23,12 @@ echo_and_eval "cd ${CLIENTDIR}"
 print_task_name "Build"
 echo_and_eval "NODE_ENV=production npm run build"
 
-print_task_name "Build complete"
+## Copy all dists to dist/
+print_task_name "Make artifacts"
+echo_and_eval "rm -rf ${PROJECTDIR}/dist"
+echo_and_eval "mkdir -p ${PROJECTDIR}/dist"
+echo_and_eval "cp -R ${SERVERDIR}/dist ${PROJECTDIR}/dist/dist-server"
+echo_and_eval "cp -R ${SERVERDIR}/data-processor/target/*.jar ${PROJECTDIR}/dist"
+echo_and_eval "cp -R ${CLIENTDIR}/dist ${PROJECTDIR}/dist/dist-dashboard"
 
+print_task_name "Build complete"
